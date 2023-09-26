1c4e57552c29c98f0c9788940e1870d1b440fa3c
nspsck <86347287+nspsck@users.noreply.github.com>
2023-09-26 13:55:55 +0200

    Add files via upload

---
+++ WEACTH723VGT6/board_init.c
@@ -0,0 +1,4 @@
+#include "py/mphal.h"
+
+void WEACT_H723ZG_board_early_init(void) {
+}
---
+++ WEACTH723VGT6/mpconfigboard.h
@@ -0,0 +1,157 @@
+#define MICROPY_HW_BOARD_NAME               "WeAct Studio MiniSTM32H7xx"
+#define MICROPY_HW_MCU_NAME                 "STM32H723VGT6"
+
+#define MICROPY_HW_ENABLE_RTC               (1)
+#define MICROPY_HW_ENABLE_RNG               (0) // RNG needs proper configuration
+#define MICROPY_HW_ENABLE_ADC               (1)
+#define MICROPY_HW_ENABLE_DAC               (1)
+#define MICROPY_HW_ENABLE_TIMER             (1)
+#define MICROPY_HW_ENABLE_SERVO             (1)
+#define MICROPY_HW_ENABLE_USB               (1)
+#define MICROPY_HW_ENABLE_SDCARD            (1)
+#define MICROPY_HW_ENABLE_MMCARD            (0)
+#define MICROPY_HW_HAS_SWITCH               (1)
+#define MICROPY_HW_HAS_FLASH                (1)
+#define MICROPY_HW_HAS_LCD                  (0)
+#define MICROPY_FATFS_EXFAT                 (1)
+
+// Flash storage config
+#define MICROPY_HW_SPIFLASH_ENABLE_CACHE            (1)
+#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE    (0)
+
+#if !MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE
+// QSPI flash for storage
+#define MICROPY_HW_OSPI_PRESCALER           (2) // 100MHz
+#define MICROPY_HW_OSPIFLASH_SIZE_BITS_LOG2 (26)
+#define MICROPY_HW_SPIFLASH_SIZE_BITS       (64 * 1024 * 1024)
+#define MICROPY_HW_OSPIFLASH_CS             (pin_B6)
+#define MICROPY_HW_OSPIFLASH_SCK            (pin_B2)
+#define MICROPY_HW_OSPIFLASH_IO0            (pin_D11)
+#define MICROPY_HW_OSPIFLASH_IO1            (pin_D12)
+#define MICROPY_HW_OSPIFLASH_IO2            (pin_E2)
+#define MICROPY_HW_OSPIFLASH_IO3            (pin_D13)
+
+// SPI flash #1, block device config
+extern const struct _mp_spiflash_config_t spiflash_config;
+extern struct _spi_bdev_t spi_bdev;
+#define MICROPY_HW_BDEV_SPIFLASH            (&spi_bdev)
+#define MICROPY_HW_BDEV_SPIFLASH_CONFIG     (&spiflash_config)
+#define MICROPY_HW_BDEV_SPIFLASH_SIZE_BYTES (MICROPY_HW_SPIFLASH_SIZE_BITS / 8)
+#define MICROPY_HW_BDEV_SPIFLASH_EXTENDED   (&spi_bdev)
+#endif
+
+#define MICROPY_BOARD_EARLY_INIT            WEACT_H723ZG_board_early_init
+
+//PLL1 400MHz
+#define MICROPY_HW_CLK_PLLM             (5)
+#define MICROPY_HW_CLK_PLLN             (160)
+#define MICROPY_HW_CLK_PLLP             (2)
+#define MICROPY_HW_CLK_PLLQ             (4)
+#define MICROPY_HW_CLK_PLLR             (2)
+#define MICROPY_HW_CLK_PLLVCI           (RCC_PLL1VCIRANGE_1)
+#define MICROPY_HW_CLK_PLLVCO           (RCC_PLL1VCOWIDE)
+#define MICROPY_HW_CLK_PLLFRAC          (0)
+
+// PLL2 200MHz
+#define MICROPY_HW_CLK_PLL2M            (5)
+#define MICROPY_HW_CLK_PLL2N            (80)
+#define MICROPY_HW_CLK_PLL2P            (2)
+#define MICROPY_HW_CLK_PLL2Q            (2)
+#define MICROPY_HW_CLK_PLL2R            (2)
+#define MICROPY_HW_CLK_PLL2VCI          (RCC_PLL2VCIRANGE_1)
+#define MICROPY_HW_CLK_PLL2VCO          (RCC_PLL2VCOWIDE)
+#define MICROPY_HW_CLK_PLL2FRAC         (0)
+
+// PLL3 120MHz
+#define MICROPY_HW_CLK_PLL3M            (5)
+#define MICROPY_HW_CLK_PLL3N            (48)
+#define MICROPY_HW_CLK_PLL3P            (2)
+#define MICROPY_HW_CLK_PLL3Q            (5)
+#define MICROPY_HW_CLK_PLL3R            (2)
+#define MICROPY_HW_CLK_PLL3VCI          (RCC_PLL3VCIRANGE_1)
+#define MICROPY_HW_CLK_PLL3VCO          (RCC_PLL3VCOWIDE)
+#define MICROPY_HW_CLK_PLL3FRAC         (0)
+
+// 4 wait states
+#define MICROPY_HW_FLASH_LATENCY            FLASH_LATENCY_2
+
+// The board has an external 32kHz crystal attached
+#define MICROPY_HW_RTC_USE_LSE              (1)
+
+// UART config
+#define MICROPY_HW_UART1_TX                 (pin_A9)
+#define MICROPY_HW_UART1_RX                 (pin_A10)
+#define MICROPY_HW_UART2_TX                 (pin_A2)
+#define MICROPY_HW_UART2_RX                 (pin_A3)
+#define MICROPY_HW_UART3_TX                 (pin_D8)
+#define MICROPY_HW_UART3_RX                 (pin_D9)
+// #define MICROPY_HW_UART4_TX                 (pin_A12)
+// #define MICROPY_HW_UART4_RX                 (pin_A11)
+#define MICROPY_HW_UART6_TX                 (pin_C6)
+#define MICROPY_HW_UART6_RX                 (pin_C7)
+#define MICROPY_HW_UART8_TX                 (pin_E1)
+#define MICROPY_HW_UART8_RX                 (pin_E0)
+
+#define MICROPY_HW_UART_REPL                PYB_UART_3
+#define MICROPY_HW_UART_REPL_BAUD           115200
+
+// I2C buses
+#define MICROPY_HW_I2C1_SCL                 (pin_B8)
+#define MICROPY_HW_I2C1_SDA                 (pin_B9)
+#define MICROPY_HW_I2C2_SCL                 (pin_B10)
+#define MICROPY_HW_I2C2_SDA                 (pin_B11)
+
+// SPI buses
+#define MICROPY_HW_SPI1_NAME                "SPIFLASH"
+#define MICROPY_HW_SPI1_NSS                 (pin_D6)
+#define MICROPY_HW_SPI1_SCK                 (pin_B3)
+#define MICROPY_HW_SPI1_MISO                (pin_B4)
+#define MICROPY_HW_SPI1_MOSI                (pin_D7)
+#define MICROPY_HW_SPI2_NAME                "SPI2"
+#define MICROPY_HW_SPI2_NSS                 (pin_B12)
+#define MICROPY_HW_SPI2_SCK                 (pin_B13)
+#define MICROPY_HW_SPI2_MISO                (pin_B14)
+#define MICROPY_HW_SPI2_MOSI                (pin_B15)
+#define MICROPY_HW_SPI4_NAME                "SPILCD"
+#define MICROPY_HW_SPI4_NSS                 (pin_E11)
+#define MICROPY_HW_SPI4_SCK                 (pin_E12)
+#define MICROPY_HW_SPI4_MISO                (pin_E13)
+#define MICROPY_HW_SPI4_MOSI                (pin_E14)
+
+// USRSW is pulled low. Pressing the button makes the input go high.
+#define MICROPY_HW_USRSW_PIN                (pin_C13)
+#define MICROPY_HW_USRSW_PULL               (GPIO_NOPULL)
+#define MICROPY_HW_USRSW_EXTI_MODE          (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED            (1)
+
+// LEDs
+#define MICROPY_HW_LED1                     (pin_E3)    // blue
+#define MICROPY_HW_LED_ON(pin)              (mp_hal_pin_high(pin))
+#define MICROPY_HW_LED_OFF(pin)             (mp_hal_pin_low(pin))
+
+// USB config
+#define MICROPY_HW_USB_FS                   (0)
+#define MICROPY_HW_USB_HS                   (1)
+#define MICROPY_HW_USB_HS_IN_FS             (1)
+#define MICROPY_HW_USB_MAIN_DEV             (USB_PHY_HS_ID)
+
+// FDCAN bus
+#define MICROPY_HW_CAN1_NAME                "FDCAN1"
+#define MICROPY_HW_CAN1_TX                  (pin_D1)
+#define MICROPY_HW_CAN1_RX                  (pin_D0)
+
+#define MICROPY_HW_CAN2_NAME                "FDCAN2"
+#define MICROPY_HW_CAN2_TX                  (pin_B13)
+#define MICROPY_HW_CAN2_RX                  (pin_B12)
+
+// SD card detect switch
+#define MICROPY_HW_SDMMC1_CMD               (pin_D2)
+#define MICROPY_HW_SDMMC1_D0                (pin_C8)
+#define MICROPY_HW_SDMMC1_D1                (pin_C9)
+#define MICROPY_HW_SDMMC1_D2                (pin_C10)
+#define MICROPY_HW_SDMMC1_D3                (pin_C11)
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_D4)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+
+void WEACT_H723ZG_board_early_init(void);
